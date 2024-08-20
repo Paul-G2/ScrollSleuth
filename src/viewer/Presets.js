b06c2651d5eef47f57601c8ce49d9c11c0973bfa
Paul-G2 <Paul-G2@users.noreply.github.com>
2024-08-19 22:40:31 -0400

    Add files via upload

--- src/viewer/Presets.js
+++ src/viewer/Presets.js
@@ -17,15 +17,15 @@ class Presets
         markerColor: new BigLime.Color(0, 128, 0, 255),
         sealBorders: false, 
         lightSet: new BigLime.LightSet({
-            ambientLight:1.118, 
+            ambientLight:0.66, 
             dirLights:[
             {
-                diffuse: 0.14, 
-                specStrength: 0.21, 
+                diffuse: 0.22, 
+                specStrength: 0.22, 
                 specExp: 2, 
                 shadowDarkness: 1.02, 
                 shadowSoftness: 0.75, 
-                dir: glMatrix.vec3.fromValues(0.73, -0.43, 0.53)
+                dir: glMatrix.vec3.fromValues(0.483, -0.420, 0.768)
             },
             {
                 diffuse: 0,
@@ -39,26 +39,26 @@ class Presets
     };
 
     static Preset2 = 
-    {
+    { 
         renderType: BigLime.RenderType.VR,
         persp: 1,
         antiAlias: true,
         interpType: BigLime.Interp3D.TriLinear,
         vrBackColor: new BigLime.Color.Black(),
-        opacityCurveStr: "[0](255, 0, 0, 0); [424](255, 0, 0, 0); [444](219, 167, 77, 150); [462](203, 185, 98, 225); [1023](255, 255, 255, 227)",
+        opacityCurveStr: "[0](255, 66, 66, 0); [390](255, 66, 66, 0); [486](219, 195, 77, 105); [505](203, 185, 98, 219); [1023](255, 255, 255, 227)",
         markerSize: 0.0075,
         markerColor: new BigLime.Color(0, 128, 0, 255),
         sealBorders: false, 
         lightSet: new BigLime.LightSet({
-            ambientLight: 0.690312, 
+            ambientLight:1.118, 
             dirLights:[
             {
-                diffuse: 0.23, 
-                specStrength: 0.0, 
-                specExp: 1, 
-                shadowDarkness: 0.51, 
+                diffuse: 0.14, 
+                specStrength: 0.21, 
+                specExp: 2, 
+                shadowDarkness: 1.02, 
                 shadowSoftness: 0.75, 
-                dir: glMatrix.vec3.fromValues(-0.05, -0.075, 0.995929)
+                dir: glMatrix.vec3.fromValues(0.73, -0.43, 0.53)
             },
             {
                 diffuse: 0,
